ce6e907f7c1746fccbbea64574a8e06e2010425c
Iván Camilo Goez Palacio <camilogoez2015@hotmail.com>
2019-10-26 10:33:05 -0500

    added gramatic class

---
+++ codigo/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Gramatica
+{
+
+    //Atributos\\
+
+    //Reglas de la gramatica
+    public List<Regla> reglas;
+
+    //Representa todas las variables utilizadas.
+    public List<char> variables;
+
+    //terminales en la gramatica
+    public List<char> terminales;
+
+    //variables posibles
+    public List<char> variablesPosibles = new List<char>(){ 'A','B','C', 'D', 'E','F','G','H','I','J','K','L','M',
+                                                                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+}
